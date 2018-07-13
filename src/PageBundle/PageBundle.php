87c95c23fa0510c998d840acf58ba7a7b5d49423
Carlos Herrera <carloshv93@gmail.com>
2018-07-13 10:44:20 -0600

    Branch para codigo de la base de datos

--- src/PageBundle/PageBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace PageBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class PageBundle extends Bundle
-{
-}
--- src/ProductBundle/ProductBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace ProductBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class ProductBundle extends Bundle
-{
-}
